96ed95fb204fb3e3734abf7d56d2ba22c097da02
Chris <chris@swibl-baseball.org>
2017-11-18 06:08:39 -0600

    added new routes

--- SWIBL-RegistrationService/index.php
+++ SWIBL-RegistrationService/index.php
@@ -8,6 +8,7 @@
 
 use swibl\services\registration\actions\ConfirmRegistrationAction;
 use swibl\services\registration\actions\DownloadRegistrationAction;
+use swibl\services\registration\actions\GetAllRegistrationAction;
 use swibl\services\registration\actions\GetRegistrationAction;
 use swibl\services\registration\actions\PostRegistrationAction;
 use swibl\services\registration\RegistrationRequestAuthorizer;
@@ -22,21 +23,35 @@ if (file_exists('bootstrap.php'))
     include_once 'bootstrap.php';
 }
 
-$config = [
-    'settings' => [
-        'displayErrorDetails' => true, 
-    ],
-];
+$c = new \Slim\Container(); //Create Your container
 
-$app = new \Slim\App($config);
+//Override the default Not Found Handler
+$c['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $c['response']
+        ->withStatus(404)
+        ->withHeader('Content-Type', 'text/html')
+        ->write('SWIBL API - Page not found');
+    };
+};
+
+// $config = [
+//     'settings' => [
+//         'displayErrorDetails' => true, 
+//     ],
+// ];
+
+$app = new \Slim\App($c);
 $app->add(new RegistrationRequestAuthorizer());
 
 // Service Routes
 $app->get('/{id}', GetRegistrationAction::class);
 $app->get('/download/{seasonid}', DownloadRegistrationAction::class);
+$app->get('/download/season/{seasonid}', DownloadRegistrationAction::class);
+$app->get('/season/{seasonid}', GetAllRegistrationAction::class);
 $app->put('/{id}/confirm/{confnum}', ConfirmRegistrationAction::class);
 $app->post('/', PostRegistrationAction::class);
-$app->delete('/', DeleteRegistrationAction::class);
+$app->delete('/{id}', DeleteRegistrationAction::class);
 $app->put('/{id}', PutRegistrationAction::class);
             
 $app->run();
