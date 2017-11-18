a6d76b3d8ad34461c09cb210286cfd7e276fa895
Chris <chris@swibl-baseball.org>
2017-11-18 06:08:31 -0600

    initial build

--- SWIBL-RegistrationService/src/services/registration/actions/DeleteRegistrationAction.php
+++ SWIBL-RegistrationService/src/services/registration/actions/DeleteRegistrationAction.php
@@ -19,16 +19,16 @@ class DeleteRegistrationAction
     public function __invoke($request, $response, $args) {
         
         $service = RegistrationService::getInstance();
-        
-        $id = $request->getAttribute("id");
-        $body = $request->getBody();
-        $content = $body->getContents();
-        
-        $content2 = $request->getParsedBody();
-        
         $logger = $service->getLogger();
         $logger->info("DELETE /" . $request->getAttribute('id') );
         $dao = RegistrationDAO::getInstance($service->getDatabase());
+        
+        $id = $request->getAttribute("id");
+//         $body = $request->getBody();
+//         $content = $body->getContents();
+        
+//         $content2 = $request->getParsedBody();
+        
         try {
             $dao->delete($id);
             $svcresponse = RegistrationServiceResponse::getInstance(200, "Record " . $id . " has been successfully deleted");
